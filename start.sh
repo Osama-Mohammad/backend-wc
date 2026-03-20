d3f34e8fc02da1b1ab4d3749670360c4f6730cfd
Ousama Mohammad <osama03mohammad@gmail.com>
2026-03-20 19:39:54 +0200

    updated the start.sh to hopefully fix the photo render

--- start.sh
+++ start.sh
@@ -1,3 +1,22 @@
+# #!/bin/bash
+# set -e
+
+# mkdir -p storage/framework/cache/data
+# mkdir -p storage/framework/sessions
+# mkdir -p storage/framework/views
+# mkdir -p bootstrap/cache
+
+# chown -R www-data:www-data storage bootstrap/cache
+# chmod -R 775 storage bootstrap/cache
+
+# php artisan config:clear
+# php artisan route:clear
+# php artisan cache:clear
+
+# php artisan migrate --force
+
+# apache2-foreground
+
 #!/bin/bash
 set -e
 
@@ -12,6 +31,9 @@ chmod -R 775 storage bootstrap/cache
 php artisan config:clear
 php artisan route:clear
 php artisan cache:clear
+
+# 🔥 ADD THIS LINE
+php artisan storage:link || true
 
 php artisan migrate --force
 
